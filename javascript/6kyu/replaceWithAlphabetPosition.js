cf49efea9614dff0174beb08cc54c491d8866f91
rachelsonnie <racheldepriest@Rachels-MacBook-Pro.local>
2020-04-16 17:09:35 -0500

    Added replaceWithAlphabetPosition.js

---
+++ javascript/6kyu/replaceWithAlphabetPosition.js
@@ -0,0 +1,21 @@
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
+function alphabetPosition(text) {
+    let answer = []
+    let alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
+    text = text.toLowerCase().replace(/[^a-zA-Z ]/g, "").replace(/ /g,'').split("")
+    text.forEach(i => answer.push(alphabet.indexOf(i) + 1))
+    return answer.join().replace(/,/g, ' ');
+}
+
+//SampleTests
+Test.assertEquals(alphabetPosition("The sunset sets at twelve o' clock."), "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11");
+Test.assertEquals(alphabetPosition("The narwhal bacons at midnight."), "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20");
