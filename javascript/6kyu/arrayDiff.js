01a06f86954e903f0347c3d57c4b47cd7b9ba3ec
rachelsonnie <racheldepriest@Rachels-MacBook-Pro.local>
2020-05-20 17:39:47 -0500

    added array.dff

---
+++ javascript/6kyu/arrayDiff.js
@@ -0,0 +1,23 @@
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+function arrayDiff(a, b) {
+    let unique = []
+    a.forEach(i => b.indexOf(i) === -1 ? unique.push(i): i)
+    return unique
+}
+
+//Sample Tests
+Test.describe("Sample tests", function() {
+    Test.it("Should pass Sample tests", function() {
+      Test.assertDeepEquals(arrayDiff([], [4,5]), [], "a was [], b was [4,5]");
+      Test.assertDeepEquals(arrayDiff([3,4], [3]), [4], "a was [3,4], b was [3]");
+      Test.assertDeepEquals(arrayDiff([1,8,2], []), [1,8,2], "a was [1,8,2], b was []");
+    });
+  }); 
